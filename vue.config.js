4b91cbccd1bdd6afd7b1ef5bc628e04584fc1b51
马祥瑞(DF研发) <maxiangrui@kingsoft.com>
2020-06-05 15:22:41 +0800

    项目结构变更

---
+++ vue.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    index: {
+        // page 的入口
+        entry: process.env.NODE_ENV === 'production' ? 'src/index.js' : 'dev/main.js',
+        // 模板来源
+        template: 'public/index.html',
+        // 输出文件名
+        filename: 'index.html'
+    },
+    devServer: {
+        // 设置端口
+        port: 18080,
+    },
+    // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。  
+    productionSourceMap: false
+}
